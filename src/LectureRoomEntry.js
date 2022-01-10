62b8fa0783ad9f6be5d91509f91d9561ae112b99
nuel <emmanuelikwuoma7@gmail.com>
2022-01-10 09:46:07 +0100

    all LTs display in add lecture room page

--- src/LectureRoomEntry.js
+++ src/LectureRoomEntry.js
@@ -1,13 +1,34 @@
-import { useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react/cjs/react.development";
 import {venues} from "./api_endpoints";
 
 import {useAuth} from "./auth/provider";
+import {MutatingDots} from "react-loader-spinner";
 
 function LectureRoomEntry() {
     const [lectureRoomCode, setLectureRoomCode] = useState("");
+    const [lectureRooms, setLectureRooms] = useState([]);
     const [capacity, setCapacity] = useState(0);
+    const [addingLT, setAddingLT] = useState(false);
     // BEARER TOKEN
     const {bearerToken} = useAuth();
+
+    function fetchLTs(venues, headers) {
+        return fetch(venues, headers)
+    }
+
+    useEffect(() => {
+        fetchLTs(venues, {
+            headers: {
+                'Content-Type': 'apllication/json',
+                Authorization: `Bearer ${bearerToken}`
+        }})
+        .then(res => res.json())
+        .then((lts) => {
+            console.log(lts)
+            setLectureRooms(lts)
+        })
+        .catch(console.error);
+    }, []);
 
     function handleCourseCodeChange(evt) {
         setLectureRoomCode(evt.target.value)
@@ -26,6 +47,7 @@ function LectureRoomEntry() {
         evt.preventDefault();
         if(!lectureRoomCode || !capacity) return;
         // call the API
+        setAddingLT(true);
         console.log("lecture room code", lectureRoomCode)
         console.log("capacity", capacity)
         fetch(venues, {
@@ -40,8 +62,27 @@ function LectureRoomEntry() {
         .then((resData) => {
             prompt(`Lecture Theatre added success`)
             handleClear()
+            setAddingLT(false)
+            return fetchLTs(venues, {
+                headers: {
+                    'Content-Type': 'apllication/json',
+                    Authorization: `Bearer ${bearerToken}`
+            }})
         })
-        .catch(console.error)
+        .then(res => res.json())
+        .then((lts) => {
+            setAddingLT(false)
+            setLectureRooms(lts);
+            handleClear()
+        })
+        .catch((err) => {
+            console.error(err);
+            setAddingLT(false);
+        })
+    }
+
+    function handleDeleteCourse() {
+
     }
 
     return (
@@ -55,42 +96,56 @@ function LectureRoomEntry() {
 
             <div class="container">
                 <div class="courseInfo">
-                    <h4>Lecture Room Name</h4>
-                    <div class="infoCard">
-                        <div class="courseStudents">
-                            <div class="ccode border">
-                                <label for="course-code">Lecture Room Code</label>
-                                <input type="text"
-                                    class="form-control"
-                                    name="courseCode"
-                                    id="courseCode"
-                                    value={lectureRoomCode}
-                                    onChange={handleCourseCodeChange}
-                                    placeholder="e.g HSLT C"
-                                    list="courses" required />
+                    <div class="container lt-display">
+                        {lectureRooms.length > 0 && (
+                            lectureRooms.map(({name, size, code}) => (
+                                <div class="row p-2" key={code}>
+                                    <div class="col border border-top-0 text-uppercase m-2 p-4">{name}</div>
+                                    <div class="col border border-top-0 text-uppercase m-2 p-4">{size}</div>
+                                    <div class="d-flex flex-column align-items-end">
+                                        <button type="button" class="btn btn-light btn-sm text-danger" onClick={handleDeleteCourse}>delete</button>
+                                    </div>
+                                </div>
+                            ))
+                        )}
+                    </div>
+                    <div class="lt-add">
+                        <h4>Lecture Room Name</h4>
+                        <div class="infoCard">
+                            <div class="courseStudents">
+                                <div class="ccode border">
+                                    <label for="course-code">Lecture Room Code</label>
+                                    <input type="text"
+                                        class="form-control"
+                                        name="courseCode"
+                                        id="courseCode"
+                                        value={lectureRoomCode}
+                                        onChange={handleCourseCodeChange}
+                                        placeholder="e.g HSLT C"
+                                        list="courses" required />
+                                </div>
+                                <div class="students border">
+                                    <label for="number-of-students">Capacity (Students)</label>
+                                    <input type="text"
+                                        class="form-control"
+                                        name="courseCode"
+                                        id="courseCode"
+                                        value={capacity}
+                                        onChange={handleCapacityChange}
+                                        placeholder="e.g 300" required />
+                                </div>
                             </div>
-                            {/* <datalist id="courses">
-                                <option value="HSLT C">HSLT C</option>
-                                <option value="BOO A">BOO A</option>
-                                <option value="1K LT">1K LT</option>
-                                <option value="FB LT">FB LT</option>
-                                <option value="AMPHI">AMPHI</option>
-                            </datalist> */}
-                            <div class="students border">
-                                <label for="number-of-students">Capacity (Students)</label>
-                                <input type="text"
-                                    class="form-control"
-                                    name="courseCode"
-                                    id="courseCode"
-                                    value={capacity}
-                                    onChange={handleCapacityChange}
-                                    placeholder="e.g 300" required />
-                            </div>
-                        </div>
-                        <div>
-                            <button class="btn-del-lect" onClick={handleClear}>Delete</button>
-                            <button class="btn btn-add-lect" onClick={handleAddLT}>Add LectureRoom</button>
+                            <div>
+                                <button class="btn-del-lect" onClick={handleClear}>Clear</button>
+                                {
+                                    !addingLT ? (
+                                        <button class="btn btn-add-lect" onClick={handleAddLT}>Add LectureRoom</button>
+                                    ): (
+                                        <MutatingDots arialLabel="loading-indicator"  color="blue" secondaryColor="grey"/>
+                                    )
+                                }
 
+                            </div>
                         </div>
                     </div>
                 </div>
